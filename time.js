9627b0bf383476e9d104423a4f2f7a947fba7b05
noelkspark <51874193+noelkspark@users.noreply.github.com>
2021-01-22 13:18:42 +0900

    added real time!

---
+++ time.js
@@ -0,0 +1,18 @@
+const clockContainer = document.querySelector(".js-clock"),
+  clockTime = clockContainer.querySelector("span");
+
+function getTime() {
+  const date = new Date();
+  const hours = date.getHours(),
+    mins = date.getMinutes();
+  clockTime.innerText = `${hours < 10 ? `0${hours}` : hours} : ${
+    mins < 10 ? `0${mins}` : mins
+  }`;
+}
+
+function init() {
+  getTime();
+  setInterval(getTime, 1000);
+}
+
+init();
